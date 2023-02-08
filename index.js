ee327d8e16620fa37dbe80c0cada17609347dfc6
Harsh Patil <harshpatil2956@gmail.com>
2023-02-08 20:13:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,51 @@
+const inputField = document.querySelector(".input-field textarea"),
+  todoLists = document.querySelector(".todoLists"),
+  pendingNum = document.querySelector(".pending-num"),
+  clearButton = document.querySelector(".clear-button");
+
+function allTasks() {
+  let tasks = document.querySelectorAll(".pending");
+
+  pendingNum.textContent = tasks.length === 0 ? "no" : tasks.length;
+
+  let allLists = document.querySelectorAll(".list");
+  if (allLists.length > 0) {
+    todoLists.style.marginTop = "20px";
+    clearButton.style.pointerEvents = "auto";
+    return;
+  }
+  todoLists.style.marginTop = "0px";
+  clearButton.style.pointerEvents = "none";
+}
+
+inputField.addEventListener("keyup", (e) => {
+  let inputVal = inputField.value.trim();
+  if (e.key === "Enter" && inputVal.length > 0) {
+    let liTag = ` <li class="list pending" onclick="handleStatus(this)">
+          <input type="checkbox" />
+          <span class="task">${inputVal}</span>
+          <i class="uil uil-trash" onclick="deleteTask(this)"></i>
+        </li>`;
+
+    todoLists.insertAdjacentHTML("beforeend", liTag); //inserting li tag inside the todolist div
+    inputField.value = ""; //removing value from input field
+    allTasks();
+  }
+});
+
+function handleStatus(e) {
+  const checkbox = e.querySelector("input"); //getting checkbox
+  checkbox.checked = checkbox.checked ? false : true;
+  e.classList.toggle("pending");
+  allTasks();
+}
+
+function deleteTask(e) {
+  e.parentElement.remove(); //getting parent element and remove it
+  allTasks();
+}
+
+clearButton.addEventListener("click", () => {
+  todoLists.innerHTML = "";
+  allTasks();
+});
